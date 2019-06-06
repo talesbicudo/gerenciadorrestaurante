cae9a6a07c19e5d1309a69efa6c61160cf680aba
tales <tales.bicudo@gmail.com>
2019-06-06 14:18:49 -0300

    input

---
+++ src/components/TheInputSearch.vue
@@ -0,0 +1,18 @@
+<template>
+    <input @change="changeHandler" v-model="value" type="search" placeholder="procurar">
+</template>
+
+<script>
+export default {
+   data: function() {
+       return {
+           value: ""
+       }
+   },
+   methods: {
+       changeHandler(){
+           this.$emit('change', this.value);
+       }
+   }
+}
+</script>
