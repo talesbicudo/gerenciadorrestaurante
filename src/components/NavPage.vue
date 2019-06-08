086416dc27b46276e688d61e2e1e117207a77945
tales <tales.bicudo@gmail.com>
2019-06-08 16:25:46 -0300

    Nav Page component proto

---
+++ src/components/NavPage.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="nav-page" :class="navPageDirClass">
+       <slot>To</slot> 
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    left: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    navPageDirClass() {
+      if (this.left) return "nav-page--left";
+      return "nav-page--right";
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.nav-page {
+  $size: 10rem;
+  position: absolute;
+  @include hoverableBg($color-grey-dark);
+  width: $size;
+  height: $size;
+  border-radius: 50%;
+  top: 50%;
+  transition: all .3s;
+  &--right {
+    right: 0;
+    transform: translateX(50%);
+  }
+  &--left {
+    left: 0;
+    transform: translateX(-50%);
+  }
+}
+</style>
