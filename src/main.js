e3746566d58e72fb0ed544de9a239aab048eacd3
tales <tales.bicudo@gmail.com>
2019-06-09 22:28:04 -0300

    currency plugin

--- src/main.js
+++ src/main.js
@@ -5,10 +5,7 @@ import store from './store'
 import { createProvider } from './vue-apollo'
 import VueCurrencyInput from 'vue-currency-input'
 
-Vue.use(VueCurrencyInput, {
-  currency: 'BRL',
-  locale: 'pt-BR'
-});
+Vue.use(VueCurrencyInput);
 
 Vue.config.productionTip = false
 const provider = createProvider()
