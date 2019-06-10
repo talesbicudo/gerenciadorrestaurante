de6db863275d9bee261cb498f581fc68b9d0a61b
tales <tales.bicudo@gmail.com>
2019-06-09 21:56:19 -0300

    Currency input usage

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { createProvider } from './vue-apollo'
+import VueCurrencyInput from 'vue-currency-input'
+
+Vue.use(VueCurrencyInput, {
+  currency: 'BRL',
+  locale: 'pt-BR'
+});
 
 Vue.config.productionTip = false
 const provider = createProvider()
