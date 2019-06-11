a5ed575659e594ad36be5d11606c4c6f3e330c8a
tales <tales.bicudo@gmail.com>
2019-06-11 13:44:04 -0300

    Close Order info

--- src/components/DetailsOrder.vue
+++ src/components/DetailsOrder.vue
@@ -2,7 +2,6 @@
     <div class="details-order">
         <h1 class="details-order__heading">Detalhes</h1>
         <div v-if="!!storeSelectedId" class="details-order__content">
-          <h3>Saldo: {{(order.totalPrice - order.totalPay) | formatPrice}}</h3>
           <div v-if="order.open" class="details-order__close-order">
              <button v-if="balanceIsZero" class="button--default" @click="closeOrder">
                {{closeOrderText}}
@@ -11,6 +10,7 @@
                {{closeOrderText}}
              </button>
           </div>
+          <h2 v-else>Conta Encerrada({{order.closedAt | time}})</h2>
           <TabbedContent  :data="tabData" />
         </div>
         <div v-else>
@@ -25,13 +25,14 @@ import StoreSelectedId from "@/mixins/StoreSelectedId";
 import FormatPrice from "@/mixins/FormatPrice";
 import CloseOrder from "@/mixins/CloseOrder";
 import BalanceOrder from "@/mixins/BalanceOrder";
+import FormatDate from "@/mixins/FormatDate";
 import TablePricePayments from "./TablePricePayments";
 import TablePriceItems from "./TablePriceItems";
 import TabbedContent from "./TabbedContent";
 import WarnCloseOrder from "./WarnCloseOrder";
 
 export default {
-  mixins: [StoreSelectedId, FormatPrice, CloseOrder, BalanceOrder],
+  mixins: [StoreSelectedId, FormatDate, FormatPrice, CloseOrder, BalanceOrder],
   components: { TabbedContent },
   apollo: {
     order: {
@@ -42,6 +43,7 @@ export default {
             totalPay @client
             totalPrice @client
             open
+            closedAt
           }
         }
       `,
