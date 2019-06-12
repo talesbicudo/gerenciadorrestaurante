e6939b531e32089713d75ce4e01818289ac3794b
tales <tales.bicudo@gmail.com>
2019-06-12 14:35:09 -0300

    fix close popup on close order success

--- src/components/WarnCloseOrder.vue
+++ src/components/WarnCloseOrder.vue
@@ -4,7 +4,7 @@
         <h2 v-if="balanceIsPositive"> Valor restante à pagar: {{balance | formatPrice}}</h2>
         <h2 v-else> Valor pago à mais: {{balanceAbs | formatPrice}}</h2>
         <h3> Continuar mesmo assim? </h3>
-        <button @click="closeOrder" class="button--default warn-close-order__button">Continuar</button>
+        <button @click="callCloseOrder" class="button--default warn-close-order__button">Continuar</button>
         <button @click="cancel" class="button--default">Cancelar</button>
     </div>
 </template>
@@ -35,9 +35,12 @@ export default {
     }
   },
   methods: {
-      cancel(){
-          this.$store.commit('popupClose');
-      }
+    cancel() {
+      this.$store.commit("popupClose");
+    },
+    callCloseOrder() {
+      this.closeOrder().then(() => this.$store.commit("popupClose"));
+    }
   }
 };
 </script>
